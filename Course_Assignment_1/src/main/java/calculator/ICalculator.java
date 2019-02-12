12bd9b6ddfd5069e39ff03b4a0466fc94e2de8ae
Alek <alek2750@gmail.com>
2019-02-12 13:02:52 +0100

    setup pages and navbar on index

---
+++ Course_Assignment_1/src/main/java/calculator/ICalculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+interface ICalculator {
+  /**
+   * Adds two numbers
+   * @param n1 The first number
+   * @param n2 Then second number
+   * @return the sum of n1+n2
+   */
+  int add(int n1, int n2);
+  /**
+   * Subtracts two numbers
+   * @param n1 The first number
+   * @param n2 Then second number
+   * @return n1 - n2
+   */
+  int sub(int n1, int n2);
+  int mul(int n1, int n2);
+  int div(int n1, int n2) throws ArithmeticException;
+}
---
+++ Course_Assignment_1/target/Course_Assignment_1-1.0-SNAPSHOT/DUIM.js
@@ -0,0 +1,108 @@
+var boys = ["Peter", "lars", "Ole"];
+var girls = ["Janne", "hanne", "Sanne"];
+
+function arrayToString(arr) {
+    var array1 = arr;
+    var æble = "";
+    for (let i = 0; i < array1.length; i++) {
+        æble = æble.concat(array1[i] + "<br>");
+    }
+    return æble;
+}
+var sorted = false;
+
+document.getElementById('boys').innerHTML = arrayToString(boys);
+document.getElementById('girls').innerHTML = arrayToString(girls);
+document.getElementById('all').innerHTML = arrayToString(boys.concat(girls));
+
+document.getElementById("removeboy").addEventListener("click", removeBoy);
+function removeBoy(event) {
+    event.preventDefault();
+    if (document.getElementById("first").checked == true) {
+        boys.shift();
+    }
+    else {
+        boys.pop();
+    }
+    document.getElementById("boys").innerHTML = arrayToString(boys);
+    document.getElementById("all").innerHTML = arrayToString(boys.concat(girls));
+}
+
+document.getElementById("addboy").addEventListener("click", addBoy);
+
+function addBoy(event) {
+    event.preventDefault();
+    var name = document.getElementById("newboy").value;
+    boys.push(name);
+    document.getElementById("newboy").value = "";
+    document.getElementById("boys").innerHTML = arrayToString(boys);
+    document.getElementById("all").innerHTML = arrayToString(boys.concat(girls));
+}
+
+document.getElementById("addgirl").addEventListener("click", addGirl);
+
+function addGirl(event) {
+    event.preventDefault();
+    var name = document.getElementById("newgirl").value;
+    girls.push(name);
+    document.getElementById("newgirl").value = "";
+    document.getElementById("girls").innerHTML = arrayToString(girls);
+    document.getElementById("all").innerHTML = arrayToString(boys.concat(girls));
+}
+
+document.getElementById("removegirl").addEventListener("click", removeGirl);
+function removeGirl(event) {
+    event.preventDefault();
+    if (document.getElementById("first").checked == true) {
+        girls.shift();
+    }
+    else {
+        girls.pop();
+    }
+    document.getElementById("girls").innerHTML = arrayToString(girls);
+    document.getElementById("all").innerHTML = arrayToString(boys.concat(girls));
+}
+
+document.getElementById("reverse").addEventListener("click", reverseAll);
+function reverseAll(event) {
+    event.preventDefault();
+    var allArr = boys.concat(girls);
+    allArr = allArr.reverse();
+    document.getElementById("all").innerHTML = arrayToString(allArr);
+}
+
+document.getElementById("sort").addEventListener("click", sortAll);
+function sortAll(event) {
+    if (sorted == false) {
+        event.preventDefault();
+        var allArr = boys.concat(girls);
+        allArr.sort(function (a, b) {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            if (a == b) return 0;
+            return a < b ? 1 : -1;
+        });
+        document.getElementById("all").innerHTML = arrayToString(allArr);
+        sorted = true;
+    }
+    else {
+        event.preventDefault();
+        var allArr = boys.concat(girls);
+        allArr.sort(function (a, b) {
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+            if (a == b) return 0;
+            return a < b ? 1 : -1;
+        });
+        allArr = allArr.reverse();
+        document.getElementById("all").innerHTML = arrayToString(allArr);
+        sorted = false;
+    }
+}
+
+/*document.getElementById('boys').innerHTML=boys;
+function showB(){
+    for (let i = 0; i < boys.length; i++) {
+        document.writeln(boys[i]);        
+    }
+}*/
